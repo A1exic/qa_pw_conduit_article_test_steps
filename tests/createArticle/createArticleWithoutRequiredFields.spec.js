bfc33df90e85e7064815a0be813739496db0356e
Alex <litvinovalex05@gmail.com>
2025-12-09 23:28:59 +0100

    All done

--- tests/createArticle/createArticleWithoutRequiredFields.spec.js
+++ tests/createArticle/createArticleWithoutRequiredFields.spec.js
@@ -18,19 +18,162 @@ test.beforeEach(async ({ page }) => {
     password: faker.internet.password(),
   };
 
-  await signUpPage.open();
-  await signUpPage.fillUsernameField(user.username);
-  await signUpPage.fillEmailField(user.email);
-  await signUpPage.fillPasswordField(user.password);
-  await signUpPage.clickSignUpButton();
-  await homePage.assertYourFeedTabIsVisible();
+  await test.step('Open sign up page', async () => {
+    await signUpPage.open();
+  });
+
+  await test.step('Fill username', async () => {
+    await signUpPage.fillUsernameField(user.username);
+  });
+
+  await test.step('Fill email', async () => {
+    await signUpPage.fillEmailField(user.email);
+  });
+
+  await test.step('Fill password', async () => {
+    await signUpPage.fillPasswordField(user.password);
+  });
+
+  await test.step('Click Sign Up', async () => {
+    await signUpPage.clickSignUpButton();
+  });
+
+  await test.step('Verify Your Feed tab visible', async () => {
+    await homePage.assertYourFeedTabIsVisible();
+  });
 });
 
-test('Creat an article without required fields', async () => {
-  await homePage.clickNewArticleLink();
+test('Create article with required and optional fields', async () => {
+  const article = {
+    title: faker.lorem.sentence(),
+    description: faker.lorem.sentence(),
+    text: faker.lorem.paragraph(),
+    tag: faker.lorem.word(),
+  };
 
-  await createArticlePage.clickPublishArticleButton();
-  await createArticlePage.assertErrorMessageContainsText(
-    'Article title cannot be empty',
-  );
+  await test.step('Click New Article link', async () => {
+    await homePage.clickNewArticleLink();
+  });
+
+  await test.step('Fill article title', async () => {
+    await createArticlePage.fillArticleTitle(article.title);
+  });
+
+  await test.step('Fill article description', async () => {
+    await createArticlePage.fillArticleDescription(article.description);
+  });
+
+  await test.step('Fill article text', async () => {
+    await createArticlePage.fillArticleText(article.text);
+  });
+
+  await test.step('Fill article tag', async () => {
+    await createArticlePage.fillArticleTag(article.tag);
+  });
+
+  await test.step('Publish article', async () => {
+    await createArticlePage.clickPublishArticleButton();
+  });
+
+  await test.step('Verify article title', async () => {
+    await createArticlePage.assertArticleTitle(article.title);
+  });
+});
+
+test('Create article without article description', async () => {
+  const article = {
+    title: faker.lorem.sentence(),
+    text: faker.lorem.paragraph(),
+    tag: faker.lorem.word(),
+  };
+
+  await test.step('Click New Article link', async () => {
+    await homePage.clickNewArticleLink();
+  });
+
+  await test.step('Fill article title', async () => {
+    await createArticlePage.fillArticleTitle(article.title);
+  });
+
+  await test.step('Fill article text', async () => {
+    await createArticlePage.fillArticleText(article.text);
+  });
+
+  await test.step('Fill article tag', async () => {
+    await createArticlePage.fillArticleTag(article.tag);
+  });
+
+  await test.step('Publish article', async () => {
+    await createArticlePage.clickPublishArticleButton();
+  });
+
+  await test.step('Verify article title', async () => {
+    await createArticlePage.assertArticleTitle(article.title);
+  });
+});
+
+test('Create article without article text', async () => {
+  const article = {
+    title: faker.lorem.sentence(),
+    description: faker.lorem.sentence(),
+    tag: faker.lorem.word(),
+  };
+
+  await test.step('Click New Article link', async () => {
+    await homePage.clickNewArticleLink();
+  });
+
+  await test.step('Fill article title', async () => {
+    await createArticlePage.fillArticleTitle(article.title);
+  });
+
+  await test.step('Fill article description', async () => {
+    await createArticlePage.fillArticleDescription(article.description);
+  });
+
+  await test.step('Fill article tag', async () => {
+    await createArticlePage.fillArticleTag(article.tag);
+  });
+
+  await test.step('Publish article', async () => {
+    await createArticlePage.clickPublishArticleButton();
+  });
+
+  await test.step('Verify error message', async () => {
+    await createArticlePage.assertErrorMessageContainsText(
+      'Article body cannot be empty',
+    );
+  });
+});
+
+test('Create article without article tag', async () => {
+  const article = {
+    title: faker.lorem.sentence(),
+    description: faker.lorem.sentence(),
+    text: faker.lorem.paragraph(),
+  };
+
+  await test.step('Click New Article link', async () => {
+    await homePage.clickNewArticleLink();
+  });
+
+  await test.step('Fill article title', async () => {
+    await createArticlePage.fillArticleTitle(article.title);
+  });
+
+  await test.step('Fill article description', async () => {
+    await createArticlePage.fillArticleDescription(article.description);
+  });
+
+  await test.step('Fill article text', async () => {
+    await createArticlePage.fillArticleText(article.text);
+  });
+
+  await test.step('Publish article', async () => {
+    await createArticlePage.clickPublishArticleButton();
+  });
+
+  await test.step('Verify article title', async () => {
+    await createArticlePage.assertArticleTitle(article.title);
+  });
 });
